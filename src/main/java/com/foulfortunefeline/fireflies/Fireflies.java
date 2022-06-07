dd785c7b29ff1bd5b5917bc08b388bc618021d20
foul-fortune-feline <foul-fortune-feline@pm.me>
2022-06-07 02:56:02 -0700

    Initial commit. Oops, all boilerplate!

---
+++ src/main/java/com/foulfortunefeline/fireflies/Fireflies.java
@@ -0,0 +1,36 @@
+/*
+ * A modification for minecraft using the quilt toolchain, adding fireflies and related items
+ * Copyright (C) 2022  Lucianna Osucha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.foulfortunefeline.fireflies;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fireflies implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod name as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
